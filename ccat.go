e01f4b30d189954076e1bdcbf07a2f54ef3b0743
Fraser Hanson <fraser.hanson@netapp.com>
2017-01-16 21:53:25 -0500

    Implement reading first 4 bytes as uint32

--- ccat.go
+++ ccat.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
   "log"
+  "bytes"
+  "encoding/binary"
   //  "github.com/gongfarmer/ade"
 )
 
@@ -11,14 +13,10 @@ func main() {
   var files = os.Args[1:]
 	for _, f := range files {
     var (
-      io *os.File
       err error
       fi os.FileInfo
+      fh *os.File
     )
-    fi, err = os.Stat(f)
-    if err != nil {
-      log.Fatal(err)
-    }
     fi, err = os.Stat(f)
     if err != nil {
       log.Fatal(err)
@@ -26,14 +24,24 @@ func main() {
     fmt.Printf("Size of file %s is %d.\n", fi.Name(), fi.Size())
 
     data := make([]byte, fi.Size())
-    count, err := io.Read(data)
+    fh, err = os.Open(f)
+    count, err := fh.Read(data)
     if err != nil {
+      fmt.Printf("Got fatal error ")
       log.Fatal(err)
     }
     fmt.Printf("read %d bytes.\n", count)
 
     sz := data[0:4]
-    fmt.Printf("read %d bytes: %q.\n", count, data[:count])
+    fmt.Printf("first 4 bytes: % X\n", sz)
+
+    var size uint32
+    buf := bytes.NewReader(data)
+    err = binary.Read(buf, binary.BigEndian, &size)
+    if err != nil {
+      fmt.Println("bytes.Read failed: ", err)
+    }
+    fmt.Printf("bytes converted to int: %d\n", size)
   }
 }
 
