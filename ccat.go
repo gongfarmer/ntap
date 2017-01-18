93e5c122ecc82208cc19302b2321a78ef9e1f19f
Fraser Hanson <fraser.hanson@netapp.com>
2017-01-17 19:22:44 -0500

    Move functionality into atom library

--- ccat.go
+++ ccat.go
@@ -2,46 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/gongfarmer/ade"
+	"log"
 	"os"
-  "log"
-  "bytes"
-  "encoding/binary"
-  //  "github.com/gongfarmer/ade"
 )
 
 func main() {
-  var files = os.Args[1:]
-	for _, f := range files {
-    var (
-      err error
-      fi os.FileInfo
-      fh *os.File
-    )
-    fi, err = os.Stat(f)
-    if err != nil {
-      log.Fatal(err)
-    }
-    fmt.Printf("Size of file %s is %d.\n", fi.Name(), fi.Size())
-
-    data := make([]byte, fi.Size())
-    fh, err = os.Open(f)
-    count, err := fh.Read(data)
-    if err != nil {
-      fmt.Printf("Got fatal error ")
-      log.Fatal(err)
-    }
-    fmt.Printf("read %d bytes.\n", count)
-
-    sz := data[0:4]
-    fmt.Printf("first 4 bytes: % X\n", sz)
-
-    var size uint32
-    buf := bytes.NewReader(data)
-    err = binary.Read(buf, binary.BigEndian, &size)
-    if err != nil {
-      fmt.Println("bytes.Read failed: ", err)
-    }
-    fmt.Printf("bytes converted to int: %d\n", size)
-  }
+	var files = os.Args[1:]
+	for _, path := range files {
+		atom, err := ade.FromFile(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(atom)
+	}
 }
-
