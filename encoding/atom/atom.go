6eb5f9787cb39f0e0215fe76a4fdc06dd8370488
Fraser Hanson <fraser.hanson@netapp.com>
2017-01-22 22:49:27 -0500

    Handle nearly all ADE types, enough to parse and print test data

--- encoding/atom/atom.go
+++ encoding/atom/atom.go
@@ -46,9 +46,47 @@ func (a Atom) String() string {
 	return output.String()
 }
 
-func (a Atom) Value() (value reflect.Value) {
-	decOpTable[adeTypeMap[a.Type]](a.Data, value)
-	return
+func (a Atom) Value() reflect.Value {
+	defer func() {
+		if r := recover(); r != nil {
+			err := fmt.Errorf("while handling atom %s:%s, %s", a.Name, a.Type, r)
+			panic(err)
+		}
+	}()
+	if !a.hasValue() {
+		panic(fmt.Errorf("Atom type %s has no value", a.Type))
+	}
+	var ptr reflect.Value = reflect.New(a.goType().Elem())
+	decoderFunc := decOpTable[adeTypeMap[a.Type]]
+	decoderFunc(a.Data, &ptr)
+	return ptr
+}
+
+// Return a go type suitable for holding a value of the given ADE Atom type
+func (a Atom) goType() reflect.Type {
+	var p reflect.Value
+	switch adeTypeMap[a.Type] {
+	case UI01:
+		p = reflect.ValueOf(new(bool))
+	case UI08, UI16, UI32, UI64:
+		p = reflect.ValueOf(new(uint64))
+	case SI08, SI16, SI32, SI64:
+		p = reflect.ValueOf(new(int64))
+	case FP32, FP64,
+		UF32, UF64, SF32, SF64,
+		UR32, UR64, SR32, SR64:
+		p = reflect.ValueOf(new(float64)) // FIXME signed float64 has smaller max value than UF64
+	case FC32, IP32, IPAD, CSTR, USTR, UUID:
+		p = reflect.ValueOf(new(string)) // FIXME signed float64 has smaller max value than UF64
+	case DATA, CNCT:
+		// FIXME: no type conversion necessary here
+		p = reflect.ValueOf(new([]byte)) // FIXME signed float64 has smaller max value than UF64
+	case ENUM:
+		p = reflect.ValueOf(new(int32))
+	default:
+		panic(fmt.Errorf("Don't know how to handle type %s", a.Type))
+	}
+	return p.Type()
 }
 
 // FIXME Rename because its not a string
@@ -64,9 +102,11 @@ func buildString(a *Atom, depth int) bytes.Buffer {
 		printableName = fmt.Sprintf("0x%+08X", a.Name)
 	}
 	fmt.Fprintf(&output, "% *s%s:%s:", depth*4, "", printableName, a.Type)
-	fmt.Fprintln(&output, a.Value())
-
-	// FIXME print data
+	if a.hasValue() {
+		fmt.Fprintln(&output, a.Value())
+	} else {
+		fmt.Fprintln(&output)
+	}
 
 	// print children
 	if a.Type == "CONT" {
@@ -76,6 +116,13 @@ func buildString(a *Atom, depth int) bytes.Buffer {
 		}
 	}
 	return output
+}
+
+func (a *Atom) hasValue() bool {
+	if a.Type == "CONT" || a.Type == "NULL" {
+		return false
+	}
+	return true
 }
 
 func (c *Atom) addChild(a *Atom) {
@@ -108,3 +155,63 @@ func FromFile(path string) (a Atom, err error) {
 	err = a.UnmarshalBinary(buf)
 	return
 }
+
+/*
+// from ADE, example of how to construct a new AtomContainer.:
+static void FINF_RegisterService(ForwarderInformationInfoPtr theModuleInfo)
+{
+	CXD_AtomContainerPtr		msgAC = NULL;
+	UINT32				rc = 0;
+
+	CXD_Atom_CreateContainer(&msgAC);
+	CXD_Atom_SetType(msgAC, CONTAINER_IS_PARENT, EVENT_SERVICE_REGISTER);
+
+	CXD_AtomPath_SetUI32(msgAC, EVENT_SERVICE_REGISTER_VERSION, 1);
+	CXD_AtomPath_SetFC32(msgAC, EVENT_SERVICE_REGISTER_SERVICEID,
+			FORWARDER_SERVICE_ID);
+	CXD_AtomPath_SetUI32(msgAC, EVENT_SERVICE_REGISTER_SERVICEVERSION
+			, FORWARDER_SERVICE_VERSION);
+	CXD_AtomPath_SetFC32(msgAC, EVENT_SERVICE_REGISTER_SERVICESCOPE,
+			FORWARDER_SERVICE_SCOPE);
+	CXD_AtomPath_SetCSTR(msgAC, EVENT_SERVICE_REGISTER_SERVICENAME,
+			FORWARDER_SERVICE_NAME);
+	CXD_AtomPath_SetFC32(msgAC, EVENT_SERVICE_REGISTER_SERVICESTATE,
+			SERVICESTATE_ENABLED);
+	CXD_AtomPath_SetUI32(msgAC, EVENT_SERVICE_REGISTER_SERVICEPROCESSID,
+			ADE_Process_GetPID());
+
+	rc = ADE_Message_PostContainer(theModuleInfo->ServicePID,
+			ADE_Process_GetNID(), &msgAC);
+	REQUIRES(rc == noErr);
+
+	theModuleInfo->ServiceRegistered = true;
+
+}
+*/
+/*
+Example of bundle attribute structure
+GODS:CONT:
+    BVER:UI32:1
+    BTIM:UI64:1
+    GOPT:CONT:
+#        "Option"
+        AVER:UI32:2
+        ATIM:UI64:1
+        AVTP:FC32:'CSTR'
+        APER:FC32:'READ'
+        AVAL:CONT:
+            0x00000000:UI32:1
+        END
+    END
+    GOVL:CONT:
+#        "Value"
+        AVER:UI32:2
+        ATIM:UI64:1
+        AVTP:FC32:'CSTR'
+        APER:FC32:'READ'
+        AVAL:CONT:
+            0x00000000:UI32:1
+        END
+    END
+END
+*/
--- encoding/atom/codec.go
+++ encoding/atom/codec.go
@@ -3,9 +3,12 @@ package atom
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
+	"io"
 	"reflect"
 )
 
+// original ADE code C-type mappings are in OSL_Types.h
 const (
 	UI01 = iota // unsigned int / bool
 	UI08        // unsigned int
@@ -36,6 +39,7 @@ const (
 	UUID        // output: DEC88E51-D85B-4425-8808-2168BC362443, input: same or hex
 	NULL        // NULL type, must have empty data section
 	CNCT        // binary data printed as hexadecimal value with leading 0x
+	CONT
 )
 
 var adeTypeMap = map[string]int{
@@ -106,7 +110,7 @@ var adeTypeMap = map[string]int{
 
 // decOp is the signature of a decoding operator for a given type.
 //type decOp func(i *decInstr, state *decoderState, v reflect.Value)
-type decOp func(buf []byte, value reflect.Value)
+type decOp func(buf []byte, value *reflect.Value)
 
 // Index by Go types.
 var decOpTable = [...]decOp{
@@ -115,78 +119,377 @@ var decOpTable = [...]decOp{
 	UI16: decUI16,
 	UI32: decUI32,
 	UI64: decUI64,
-	//	SI01: decSI01,
-	//	SI08: decSI08,
-	//	SI16: decSI16,
-	//	SI32: decSI32,
-	//	SI64: decSI64,
-	//	FP32: decFP32,
-	//	FP64: decFP64,
-	//	UF32: decUF32,
-	//	UF64: decUF64,
-	//	SF32: decSF32,
-	//	SF64: decSF64,
-	//	UR32: decUR32,
-	//	SR32: decSR32,
-	//	UR64: decUR64,
-	//	SR64: decSR64,
-	//	FC32: decFC32,
-	//	IP32: decIP32,
+	SI08: decSI08,
+	SI16: decSI16,
+	SI32: decSI32,
+	SI64: decSI64,
+	FP32: decFP32,
+	FP64: decFP64,
+	UF32: decUF32,
+	UF64: decUF64,
+	SF32: decSF32,
+	SF64: decSF64,
+	UR32: decUR32,
+	SR32: decSR32,
+	UR64: decUR64,
+	SR64: decSR64,
+	FC32: decFC32,
+	IP32: decIP32,
 	//	IPAD: decIPAD,
-	//	CSTR: decCSTR,
-	//	USTR: decUSTR,
-	//	DATA: decDATA,
-	//	CNCT: decDATA,
-	//	ENUM: decENUM,
+	CSTR: decCSTR,
+	USTR: decUSTR,
+	DATA: decDATA,
+	CNCT: decDATA,
+	ENUM: decENUM,
 	//	UUID: decUUID,
 	//	NULL: decNULL,
 }
 
 /**********************************************************/
 
-func decUI01(buf []byte, value reflect.Value) {
-	var v uint64
+func decUI01(buf []byte, value *reflect.Value) {
+	var v uint32
 	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
-	if err != nil {
+	if err != io.EOF && err != nil {
 		panic(err)
 	}
-	if v == 0 {
-		value.SetBool(false)
-	} else {
-		value.SetBool(true)
+	t := false
+	if v == 1 {
+		t = true
 	}
-
+	*value = reflect.ValueOf(t)
+}
+func decUI08(buf []byte, value *reflect.Value) {
+	var v uint8
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decUI16(buf []byte, value *reflect.Value) {
+	var v uint16
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decUI32(buf []byte, value *reflect.Value) {
+	var v uint32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decUI64(buf []byte, value *reflect.Value) {
+	var v uint64
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decSI08(buf []byte, value *reflect.Value) {
+	var v int8
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decSI16(buf []byte, value *reflect.Value) {
+	var v int16
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decSI32(buf []byte, value *reflect.Value) {
+	var v int32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decSI64(buf []byte, value *reflect.Value) {
+	var v int64
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decFP32(buf []byte, value *reflect.Value) {
+	var v int32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decFP64(buf []byte, value *reflect.Value) {
+	var v int64
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decUF32(buf []byte, value *reflect.Value) {
+	var integer, fractional uint16
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &integer)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &fractional)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(integer)
+	v += (1.0 / float64(fractional))
+	*value = reflect.ValueOf(v)
+}
+func decUF64(buf []byte, value *reflect.Value) {
+	var integer, fractional uint32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &integer)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &fractional)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(integer)
+	v += (1.0 / float64(fractional))
+	*value = reflect.ValueOf(v)
+}
+func decSF32(buf []byte, value *reflect.Value) {
+	var integer, fractional int16
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &integer)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &fractional)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(integer)
+	v += (1.0 / float64(fractional))
+	*value = reflect.ValueOf(v)
+}
+func decSF64(buf []byte, value *reflect.Value) {
+	var integer, fractional int32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &integer)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &fractional)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(integer)
+	v += (1.0 / float64(fractional))
+	*value = reflect.ValueOf(v)
+}
+func decUR32(buf []byte, value *reflect.Value) {
+	var numerator, denominator uint16
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &numerator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &denominator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(numerator) / float64(denominator)
+	*value = reflect.ValueOf(v)
+}
+func decUR64(buf []byte, value *reflect.Value) {
+	var numerator, denominator uint32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &numerator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &denominator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(numerator) / float64(denominator)
+	*value = reflect.ValueOf(v)
+}
+func decSR32(buf []byte, value *reflect.Value) {
+	var numerator, denominator int16
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &numerator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &denominator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(numerator) / float64(denominator)
+	*value = reflect.ValueOf(v)
+}
+func decSR64(buf []byte, value *reflect.Value) {
+	var numerator, denominator int32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &numerator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, &denominator)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var v float64 = float64(numerator) / float64(denominator)
+	*value = reflect.ValueOf(v)
+}
+func decFC32(buf []byte, value *reflect.Value) {
+	var v [4]byte
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	var s = string(v[:])
+	*value = reflect.ValueOf(s)
+}
+func decIP32(buf []byte, value *reflect.Value) {
+	var v [4]byte
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	s := fmt.Sprintf("%d.%d.%d.%d", v[0], v[1], v[2], v[3])
+	*value = reflect.ValueOf(s)
+}
+func decENUM(buf []byte, value *reflect.Value) {
+	var v int32
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decCSTR(buf []byte, value *reflect.Value) {
+	var v []byte
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
+}
+func decUSTR(buf []byte, value *reflect.Value) {
+	var v []byte
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	s := string(v)
+	*value = reflect.ValueOf(s)
+}
+func decDATA(buf []byte, value *reflect.Value) {
+	var v []byte
+	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
+	if err != io.EOF && err != nil {
+		panic(err)
+	}
+	*value = reflect.ValueOf(v)
 }
 
-func decUI08(buf []byte, value reflect.Value) {
-	var v uint64
-	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
-	if err != nil {
-		panic(err)
-	}
-	value.SetUint(v)
+/**********************************************************/
+
+// Called on a container, create atom at the given path if not exist, and set to given value
+func (a Atom) SetUI32(path string, value uint32) (err error) {
+	return
 }
-func decUI16(buf []byte, value reflect.Value) {
-	var v uint64
-	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
-	if err != nil {
-		panic(err)
+
+// bounds checking is implicit since uint32 cannot hold invalid values for UI32
+func (a Atom) SetValue(adeType int, value interface{}) (err error) {
+	v := reflect.ValueOf(value)
+	switch adeType {
+	case CONT, NULL:
+		err = fmt.Errorf("ADE type %s cannot take a value", adeType)
+	case UI01:
+		switch v.Kind() {
+		case reflect.Bool:
+			if v.Bool() == true {
+				binary.BigEndian.PutUint32(a.Data, uint32(1))
+			} else {
+				binary.BigEndian.PutUint32(a.Data, uint32(1))
+			}
+		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fallthrough
+		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if v.Uint() == 0 || v.Uint() == 1 {
+				v := uint32(v.Uint())
+				binary.BigEndian.PutUint32(a.Data, v)
+			} else {
+				err = fmt.Errorf("Invalid value for type %s: %d", adeType, v)
+			}
+		default:
+			err = fmt.Errorf("Invalid go type (%s) given for conversion to ADE type %d", v.Type(), adeType)
+		}
+	case UI08:
+		switch v.Kind() {
+		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fallthrough
+		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v := v.Uint()
+			if v >= 0 && v <= 0xFF {
+				a.Data[0] = byte(v)
+			} else {
+				err = fmt.Errorf("Invalid value for type %s: %d", adeType, v)
+			}
+		default:
+			err = fmt.Errorf("Invalid go type (%s) given for conversion to ADE type %d", v.Type(), adeType)
+		}
+	case UI16:
+		switch v.Kind() {
+		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fallthrough
+		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v := v.Uint()
+			if v >= 0 && v <= 0xFFFF {
+				binary.BigEndian.PutUint16(a.Data, uint16(v))
+			} else {
+				err = fmt.Errorf("Invalid value for type %s: %d", adeType, v)
+			}
+		default:
+			err = fmt.Errorf("Invalid go type (%s) given for conversion to ADE type %d", v.Type(), adeType)
+		}
+	case UI32:
+		switch v.Kind() {
+		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fallthrough
+		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v := v.Uint() // FIXME: what if it was an SINT originally?
+			if v >= 0 && v <= 0xFFFFFFFF {
+				binary.BigEndian.PutUint32(a.Data, uint32(v))
+			} else {
+				err = fmt.Errorf("Invalid value for type %s: %d", adeType, v)
+			}
+		default:
+			err = fmt.Errorf("Invalid go type (%s) given for conversion to ADE type %d", v.Type(), adeType)
+		}
+	case UI64:
+		switch v.Kind() {
+		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fallthrough
+		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v := v.Uint() // FIXME: what if it was an SINT originally?
+			if v >= 0 {
+				binary.BigEndian.PutUint64(a.Data, uint64(v))
+			} else {
+				err = fmt.Errorf("Invalid value for type %s: %d", adeType, v)
+			}
+		default:
+			err = fmt.Errorf("Invalid go type (%s) given for conversion to ADE type %d", v.Type(), adeType)
+		}
+	default:
+		err = fmt.Errorf("unknown ADE type %d", adeType)
 	}
-	value.SetUint(v)
-}
-func decUI32(buf []byte, value reflect.Value) {
-	var v uint64
-	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
-	if err != nil {
-		panic(err)
-	}
-	value.SetUint(v)
-}
-func decUI64(buf []byte, value reflect.Value) {
-	var v uint64
-	err := binary.Read(bytes.NewReader(buf), binary.BigEndian, &v)
-	if err != nil {
-		panic(err)
-	}
-	value.SetUint(v)
+	return
 }
